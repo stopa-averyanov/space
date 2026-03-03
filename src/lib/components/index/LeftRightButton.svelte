0134236e55e52529b649ec4faaefa22ea5d6230a
Stopa Averyanov <stopa.averyanov@gmail.com>
2026-03-03 22:48:18 +0300

    feat: add animations to buttons on index

--- src/lib/components/index/LeftRightButton.svelte
+++ src/lib/components/index/LeftRightButton.svelte
@@ -1,14 +1,37 @@
 <script lang='ts'>
 import { asset } from "$app/paths";
 
-
 const props : { clickLeft : () => void, clickRight : () => void } = $props();
 
+let leftCircle : HTMLDivElement;
+let rigthCircle : HTMLDivElement;
+
+function clickLeft() {
+
+    props.clickLeft();
+    leftCircle.classList.add('white-circle');
+    leftCircle.style.animation = 'none';
+    leftCircle.offsetHeight;
+    leftCircle.style.animation = '';
+}
+function clickRight() {
+
+    props.clickRight();
+    rigthCircle.classList.add('white-circle');
+    rigthCircle.style.animation = 'none';
+    rigthCircle.offsetHeight;
+    rigthCircle.style.animation = '';
+}
+
 </script>
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div class='buttons'>
-    <div class='button-left' onclick={props.clickLeft} style={`background-image: url('${asset('/arrow-left.svg')}')`}>
+    <div class='button-left' onclick={clickLeft} style={`background-image: url('${asset('/arrow-left.svg')}')`}>
+        <div bind:this={leftCircle}></div>
     </div>
-    <div class='button-right' onclick={props.clickRight} style={`background-image: url('${asset('/arrow-left.svg')}')`}>
+    <div class='button-right' onclick={clickRight} style={`background-image: url('${asset('/arrow-left.svg')}')`}>
+        <div bind:this={rigthCircle}></div>
     </div>
 </div>
 
@@ -28,5 +51,24 @@ const props : { clickLeft : () => void, clickRight : () => void } = $props();
     min-height: 72px;
     background-size: cover;
     transform: scaleX(-1);
+}
+div:global(.white-circle) {
+    background-color: white;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    border-radius: 50%;
+
+    animation-name: fade-out;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+}
+@keyframes fade-out {
+    0% {
+        opacity: 0.2;
+    }
+    100% {
+        opacity: 0;
+    }
 }
 </style>
